4bd8d418253ca1465e149c6c534eb1d964fc6c75
Robert Sabirov <rasabirov@avito.ru>
2025-12-21 09:39:57 +0300

    feat: add config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,40 @@
 package config
 
-type Config struct{}
+import (
+	"flag"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	RunAddress           string
+	DatabaseURI          string
+	AccrualSystemAddress string
+}
 
 func NewConfig() *Config {
-	return &Config{}
+	return &Config{
+		RunAddress:           ":8081",
+		DatabaseURI:          "postgres://localhost/gophermart?sslmode=disable",
+		AccrualSystemAddress: "http://localhost:8080",
+	}
 }
 
 func (c *Config) Parse() {
+	flag.StringVar(&c.RunAddress, "a", c.RunAddress, "Address and port to run the server on")
+	flag.StringVar(&c.DatabaseURI, "d", c.DatabaseURI, "Database connection URI")
+	flag.StringVar(&c.AccrualSystemAddress, "r", c.AccrualSystemAddress, "Address of the accrual system")
+
+	flag.Parse()
+
+	// перезатрем если указаны переменные окружения
+	if addr := os.Getenv("RUN_ADDRESS"); addr != "" {
+		c.RunAddress = addr
+	}
+	if dbURI := os.Getenv("DATABASE_URI"); dbURI != "" {
+		c.DatabaseURI = dbURI
+	}
+	if accrualAddr := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); accrualAddr != "" {
+		c.AccrualSystemAddress = strings.TrimRight(accrualAddr, "/")
+	}
 }
