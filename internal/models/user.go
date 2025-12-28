cd2443e0c23702b660a9aca25547acafd94785e4
Robert Sabirov <rasabirov@avito.ru>
2025-12-28 10:51:29 +0300

    fix: update user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 var (
 	ErrUserAlreadyExists = errors.New("user already exists")
@@ -8,8 +11,8 @@ var (
 )
 
 type User struct {
-	ID           int64  `db:"user_id"`
-	Login        string `db:"login"`
-	PasswordHash string `db:"password_hash"`
-	CreatedAt    int64  `db:"created_at"`
+	ID           int64     `db:"user_id"`
+	Login        string    `db:"login"`
+	PasswordHash string    `db:"password_hash"`
+	CreatedAt    time.Time `db:"created_at"`
 }
