60220f415f8df7d2bee92d643a634e1269fb9dd1
Robert Sabirov <rasabirov@avito.ru>
2026-01-04 12:22:58 +0300

    fix: remove export auth context key

--- internal/services/auth_context/auth_context.go
+++ internal/services/auth_context/auth_context.go
@@ -4,14 +4,14 @@ import "context"
 
 type ContextKey string
 
-const ContextUserIDKey ContextKey = "userID"
+const contextUserIDKey ContextKey = "userID"
 
 func CreateContextWithUserID(ctx context.Context, userID int64) context.Context {
-	return context.WithValue(ctx, ContextUserIDKey, userID)
+	return context.WithValue(ctx, contextUserIDKey, userID)
 }
 
 func GetUserIDFromContext(ctx context.Context) (userID int64) {
-	userIDInterface := ctx.Value(ContextUserIDKey)
+	userIDInterface := ctx.Value(contextUserIDKey)
 
 	if userIDInterface != nil {
 		userID = userIDInterface.(int64)
